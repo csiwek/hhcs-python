82dcc661ee3ebcf6dd1dbfb2c4bb62b63e5330e8
Cezay Siwek <cezary.siwek@gmail.com>
2016-08-24 08:56:15 +0000

    Session support

--- modules/web.py
+++ modules/web.py
@@ -3,6 +3,8 @@
 import sys, os, string
 from twisted.web import server, resource
 from twisted.internet import threads, reactor, defer
+from twisted.web.server import Session
+from twisted.python.components import registerAdapter
 
 #from concurrent.futures.thread import ThreadPoolExecutor
 import pprint
@@ -27,8 +29,22 @@ class Process(resource.Resource):
     def render_GET(self, request):
 	self.log.info("Serving: %s " % self.name)
 	loader = template.Loader("./template")
-	return loader.load("index.html").generate()
+	if self.name == "" or self.name=="index.html":
+		return loader.load("index.html").generate(sid=request.getSession().uid)
+	elif self.name == "login.html":
+		request.getSession().expire()
+		return loader.load(self.name).generate()
+	else:
+		return loader.load(self.name).generate()
 
+    def render_POST(self, request):
+	self.log.info("Serving POST: %s " % self.name)
+	if self.name == "login.html":
+		#registerAdapter(Session)
+		session = request.getSession()
+	request.redirect("index.html")
+        request.finish()
+	return server.NOT_DONE_YET
 #    def render(self, request):
 #	self.log.info("Serving: " . self.name)
 #	loader = template.Loader("./template")
